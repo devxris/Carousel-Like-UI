1dbab12bae4d4ac9e078f62a124f412ccb6d7dd0
devxris <chuwei0915@gmail.com>
2017-10-12 09:25:43 +0800

    Simple UI and setup

---
+++ Carousel-Like UI/Controller/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Carousel-Like UI
+//
+//  Created by Chris Huang on 12/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
+
+	@IBOutlet weak var backgroundImageView: UIImageView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		backgroundImageView.image = #imageLiteral(resourceName: "cloud")
+		let blurEffect = UIBlurEffect(style: .dark)
+		let blurEffectView = UIVisualEffectView(effect: blurEffect)
+		blurEffectView.frame = view.bounds
+		backgroundImageView.addSubview(blurEffectView)
+	}
+}
